8827f8859d6562ae1e89a41617f6a302fe5729ef
Zachary Moseti <mosetizachary001@gmail.com>
2021-10-18 23:05:36 +0300

    Creating Components

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -1,12 +1,11 @@
-
+import Header from './components/Header';
 function App() {
   const name = "Developer Moseti";
   const x = false;
 
   return (
     <div className="App">
-      <h1>Hello { name } </h1>
-      <p> Hello { x ? 'Yes' : 'No' }</p>
+      <Header />
     </div>
   );
 }
