52f885292fe71bc19354c2667ae71bc915692343
Zachary Moseti <mosetizachary001@gmail.com>
2021-10-18 23:07:47 +0300

    Class based components in React

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -1,13 +1,19 @@
+import React from 'react';
 import Header from './components/Header';
-function App() {
-  const name = "Developer Moseti";
-  const x = false;
+// function App() {
+//   const name = "Developer Moseti";
+//   const x = false;
 
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  );
+//   return (
+//     <div className="App">
+//       <Header />
+//     </div>
+//   );
+// }
+
+class App extends React.Component {
+  render() {
+    return <h1>Hello from a class</h1>
+  }
 }
-
 export default App;
