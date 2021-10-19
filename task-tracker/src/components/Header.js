fa326193d2ea389a70f8a7d9626080b45a70d98a
Zachary Moseti <mosetizachary001@gmail.com>
2021-10-19 18:02:58 +0300

    Button component

--- task-tracker/src/components/Header.js
+++ task-tracker/src/components/Header.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Button from '../components/Button';
 
 const Header = ({ title }) => {
     return (
-        <header>
-            <h1 style={ h1Style }>Task Tracker App</h1>
-            <p>This is { title } </p>
+        <header className="header">
+            <h1>{ title }</h1>
+            <Button color="green" text="Add Task"/>
+            <Button color="grey" text="Update Task"/>
+            <Button color="red" text="Delete Task"/>
+         
         </header>
     )
 }
@@ -17,8 +21,8 @@ Header.propTypes = {
     title: PropTypes.string.isRequired, 
 }
 
-const h1Style = {
-    color:'green',
-    backgroundColor:'black',
-}
+// const h1Style = {
+//     color:'green',
+//     backgroundColor:'black',
+// }
 export default Header;
